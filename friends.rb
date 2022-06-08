df38c15a4dde10a01cd30778e505088e4add72a4
Gavin Carew <gjcarew@gmail.com>
2022-06-08 13:27:00 -0700

    Update friends.rb with if statement

---
+++ friends.rb
@@ -0,0 +1,12 @@
+
+best_friend = "Adam"
+#
+another_friend = "JT"
+
+if best_friend == "Adam"
+  puts "You are my best friend!"
+elsif best_friend == "JT"
+  puts "You are another good friend"
+else
+  puts "You are another friend"
+end
